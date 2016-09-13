65948f81d5131d6dc3b9e0680433e0f6102b03f5
rf222cz <rf222cz@student.lnu.se>
2016-09-13 11:30:40 +0200

    staring on assignment 2 :dancer:

---
+++ src/rf222cz_assign2/ArrayIntList.java
@@ -0,0 +1,7 @@
+package rf222cz_assign2;
+
+/**
+ * Created by ymafr on 2016-09-13.
+ */
+public class ArrayIntList extends da1031.AbstractIntCollection implements da1031.IntList {
+}
---
+++ src/rf222cz_assign2/ArrayIntStack.java
@@ -0,0 +1,7 @@
+package rf222cz_assign2;
+
+/**
+ * Created by ymafr on 2016-09-13.
+ */
+public class ArrayIntStack extends da1031.AbstractIntCollection implements da1031.IntStack {
+}
