be598c2e353178bafc8ce36a60c050fabe6d1aa3
rf222cz <rf222cz@student.lnu.se>
2016-09-16 11:32:07 +0200

    Done with queueTest :goat:

--- src/rf222cz_assign2/Exercise4_Queue/QueueTest.java
+++ src/rf222cz_assign2/Exercise4_Queue/QueueTest.java
@@ -7,6 +7,10 @@ import org.junit.internal.TextListener;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.JUnitCore;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by ymafr on 2016-09-16.
  */
@@ -30,7 +34,63 @@ public class QueueTest {
     }
 
     @Test
-    public void testEnqueue(){
-        
+    public void testEnqueueNSize(){
+        System.out.println("EnqueueNSize()");
+        Queue q = enqueueMany(10000000);
+        assertEquals(10000000-1, q.size());//Minus one because EnqueueMany start with one
+    }
+
+    @Test
+    public void testDequeueNEmpty(){
+        exception.expect(IndexOutOfBoundsException.class);
+        System.out.println("DequeueNEmpty()");
+
+        //Force exception, dequeue on empty array
+        Queue q = new Queue();
+        assertTrue(q.isEmpty());
+        q.dequeue();
+
+        q = enqueueMany(10);
+        int firstSize = q.size();
+        q.dequeue();
+        assertNotEquals(firstSize, q.size());
+    }
+
+    @Test
+    public void testFirst(){
+        exception.expect(IndexOutOfBoundsException.class);
+        System.out.println("First()");
+
+        //Force exception with first on empty array
+        Queue q = new Queue();
+        assertTrue(q.isEmpty());
+        q.first();
+
+        q = enqueueMany(10);
+        assertEquals(1, q.first());
+
+    }
+
+    @Test
+    public void testLast(){
+        exception.expect(IndexOutOfBoundsException.class);
+        System.out.println("Last()");
+
+        //Force exception with first on empty array
+        Queue q = new Queue();
+        assertTrue(q.isEmpty());
+        q.last();
+
+        q = enqueueMany(10);
+        assertEquals(10, q.last());
+
+    }
+
+    //Help method
+    private Queue enqueueMany(int size) {
+        Queue q = new Queue();
+        for (int i = 1; i < size; i++)
+            q.enqueue(i);
+        return q;
     }
 }
